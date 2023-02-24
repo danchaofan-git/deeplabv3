a60a1155b9257db9f61391d99977eb32b9c89fd3
WeiZeLin <1793444169@qq.com>
2023-02-24 19:33:30 +0800

    add kaggle format dataloader

--- my_dataset.py
+++ my_dataset.py
@@ -98,6 +98,51 @@ class CityScapeSegmentation(data.Dataset):
         return batched_imgs, batched_targets
 
 
+class KaggleCityScapeSegmentation(data.Dataset):
+    def __init__(self, voc_root, txt_root, transforms=None, images_name: str = "train.txt", labels_name: str = "trainLabels.txt"):
+        super(KaggleCityScapeSegmentation, self).__init__()
+
+        image_path, label_path = os.path.join(txt_root, images_name), \
+                                 os.path.join(txt_root, labels_name)
+        assert os.path.exists(image_path), "file '{}' does not exist.".format(image_path)
+        assert os.path.exists(label_path), "file '{}' does not exist.".format(label_path)
+        with open(os.path.join(image_path), "r") as f:
+            image_names = [x.strip() for x in f.readlines() if len(x.strip()) > 0]
+        with open(os.path.join(label_path), "r") as f:
+            label_names = [x.strip() for x in f.readlines() if len(x.strip()) > 0]
+
+        self.images = [os.path.join(voc_root, x) for x in image_names]
+        self.masks = [os.path.join(voc_root, x) for x in label_names]
+        assert (len(self.images) == len(self.masks))
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is the image segmentation.
+        """
+        img = Image.open(self.images[index]).convert('RGB')
+        target = Image.open(self.masks[index])
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.images)
+
+    @staticmethod
+    def collate_fn(batch):
+        images, targets = list(zip(*batch))
+        batched_imgs = cat_list(images, fill_value=0)
+        batched_targets = cat_list(targets, fill_value=255)
+        return batched_imgs, batched_targets
+
+
 def cat_list(images, fill_value=0):
     max_size = tuple(max(s) for s in zip(*[img.shape for img in images]))
     batch_shape = (len(images),) + max_size
